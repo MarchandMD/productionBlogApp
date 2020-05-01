b8ea7184d03fe1a58aa25552710890bf015067d4
Michael Marchand <michaeldavidmarchand@gmail.com>
2020-04-30 22:18:08 -0600

    finished through section 5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,68 @@
 class HTMLWithPants < Redcarpet::Render::HTML
-    include Redcarpet::Render::SmartyPants
+  include Redcarpet::Render::SmartyPants
 end
 
-
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
-    def index
-        @articles = Article.all
+  def show
+    @article = Article.find(params[:id])
+
+    # renderer = Redcarpet::Render::HTML.new(filter_html: true)
+    renderer = HTMLWithPants.new(filter_html: true)
+
+    extensions = {
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
+      autolink: true,
+      strikethrough: true,
+    }
+
+    @display_text = Redcarpet::Markdown.new(renderer, extensions)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @article = Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
 
-        
-
-        # renderer = Redcarpet::Render::HTML.new(filter_html: true)
-        renderer = HTMLWithPants.new(filter_html: true)
-
-        extensions = {
-            no_intra_emphasis: true,
-            tables: true,
-            fenced_code_blocks: true,
-            disable_indented_code_blocks: true, 
-            autolink: true, 
-            strikethrough: true
-        }
-
-        @display_text = Redcarpet::Markdown.new(renderer, extensions)
-
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-    def new
-    end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
 
-    def create
-        @article = Article.new(article_params)
- 
-        @article.save
-        redirect_to @article
-    end
+  private
 
-    private
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- notes.md
+++ notes.md
@@ -16,3 +16,31 @@ I mean, if that's something that I want to do, I think I can take the time to do
 BBut I'd like to continue forward with the tutorial, the getting started application. So I'll do taht. 
 
 Ok, switching to a "dev" branch locally....
+
+## 5.13 Deleting Articles
+>..the route for deleting articles as per output of `rails routes` is: 
+
+```
+DELETE /articles/:id(.:format)      articles#destroy
+```
+
+>The `delete` routing method should be used for routes that destroy resources. 
+
+What? the `delete` routing method should be used for routes that destroy resources. I don't really know what that means specifically, but it makes some sense to me. Just keep going. 
+
+>if this was left as a typical `get` route, it could be possible for people to craft malicious URLs like this: 
+
+```html
+<a href="http://example.com/articles/1/destroy">look at this cat!!</a>
+```
+
+>I use the `delete` method for destroying resources, and this route is mapped to the `Destroy` method/action inside the `app/controllers/articles_controller.rb` which doesn't exist yet. 
+
+THE Controller exists. What doesn't exist is the `#destroy` method. 
+
+So, yeah, this is like a pre-defined route because it's a resourceful route. I mean, it's available since the controller and/or resource was created, yeah? Something like that. 
+
+So, when were the routes created?  
+
+So back in the `Getting Up and Running` section, there was discussion about the `#resources` method. I didn't realize it was a method at the time, but that's exactly what it was...and that meethod created the routes. 
+
